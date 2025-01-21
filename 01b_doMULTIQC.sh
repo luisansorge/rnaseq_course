3a1c990c401625104f4b210b013425f2bd6ed36e
Luis Ansorge <lansorge@login8.hpc.binf.unibe.ch>
2025-01-21 14:57:28 +0100

    Summary of the fastqc output using MultiQC 1.11

--- 01b_doMULTIQC.sh
+++ 01b_doMULTIQC.sh
@@ -5,10 +5,10 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=pibu_el8
 
-#Load MultiQC to create a report of the fastqc results in order to compare the quality of reads across all samples
+# Load MultiQC to create a report of the fastqc results in order to compare the quality of reads across all samples
 module load MultiQC/1.11-foss-2021a
 
-## Inputs all files from the fastq analysis in the specified directory (/data/users/lansorge/rnaseq_course/QCoutput)
+# Inputs all files from the fastq analysis in the specified directory (/data/users/lansorge/rnaseq_course/QCoutput)
 FASTQC_RESULTS_DIR="/data/users/lansorge/rnaseq_course/QCoutput"
 MULTIQC_OUTPUT_DIR="/data/users/lansorge/rnaseq_course/multiQC_output"
 
@@ -18,4 +18,5 @@ mkdir $MULTIQC_OUTPUT_DIR
 # The following command runs MultiQC
 # -o: defines the output directory
 # --interactive enables the creation of a HTML report
+
 multiqc $FASTQC_RESULTS_DIR -o $MULTIQC_OUTPUT_DIR --interactive --title "MultiQC Report - Raw Data"
